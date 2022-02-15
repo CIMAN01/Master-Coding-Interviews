0f2291eae54a7df3c07c28e6a2bd4ac274ff763e
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2022-02-14 16:57:51 -0800

    Update BinaryTreeLevelOrderTraversal.java

--- FAANG-Interviews/8. Binary Trees/BinaryTreeLevelOrderTraversal.java
+++ FAANG-Interviews/8. Binary Trees/BinaryTreeLevelOrderTraversal.java
@@ -7,9 +7,9 @@ Given the root of a binary tree, return the level order traversal of its nodes' 
 
 Example 1:
 
-          3
+                  3
 		/   \
-	   9    20
+	       9    20
 		   /  \
 		  15   7
 
